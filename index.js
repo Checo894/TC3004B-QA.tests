8cb487b4cb1f9b852ea7ff74d1adfdee26541b8d
Sergio Gutiérrez <sergut046@hotmail.com>
2025-02-17 07:21:10 -0600

    v1

---
+++ index.js
@@ -0,0 +1,16 @@
+function calculate() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    let result;
+    result = parseFloat(num1) + parseFloat(num2);
+
+    document.getElementById('result').innerHTML = "Result: " + result;
+}
+
+function resetFields() {
+    document.getElementById('num1').value = '';
+    document.getElementById('num2').value = '';
+    document.getElementById('operator').innerHTML = '+';
+    document.getElementById('result').innerHTML = 'Result: ';
+}
