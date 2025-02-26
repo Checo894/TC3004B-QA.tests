8abfca901403fac222a34425a5a9074fca6ff698
Sergio E. Gutiérrez <128638772+Checo894@users.noreply.github.com>
2025-02-26 09:55:46 -0600

    Update addtwoletters.spec.js

--- tests/addtwoletters.spec.js
+++ tests/addtwoletters.spec.js
@@ -4,7 +4,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 const fs = require('fs')
 
-describe('test1', function() {
+describe('Add two letters', function() {
   this.timeout(30000)
   let driver
   let vars
