f4887c5c0f898bbfddc6e8ba548285529aec5cbb
Checo894 <a01068505@tec.mx>
2025-03-10 08:25:45 -0600

    back & front connection

--- backend/src/main/java/com/example/calculator/CalculatorController.java
+++ backend/src/main/java/com/example/calculator/CalculatorController.java
@@ -1,10 +1,11 @@
 package com.example.calculator;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@CrossOrigin(origins = "http://localhost:8000")
 @RestController
 public class CalculatorController {
     private static final String template = "Result: %s";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,27 @@
 function calculateAdd() {
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-
-    let result;
-    result = parseFloat(num1) + parseFloat(num2);
-
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
     document.getElementById('operator').innerHTML = '+';
-    document.getElementById('result').innerHTML = "Result: " + result;
+
+    fetch(`http://localhost:8080/add?num1=${num1}&num2=${num2}`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("result").textContent = data.result;
+    })
+    .catch(error => console.error("Error en la operación:", error));
 }
 
 function calculateSub() {
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-
-    let result;
-    result = parseFloat(num1) - parseFloat(num2);
-
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
     document.getElementById('operator').innerHTML = '-';
-    document.getElementById('result').innerHTML = "Result: " + result;
+
+    fetch(`http://localhost:8080/sub?num1=${num1}&num2=${num2}`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("result").textContent = data.result;
+    })
+    .catch(error => console.error("Error en la operación:", error));
 }
 
 function resetFields() {
